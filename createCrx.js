6a213fcf88f1e66887a724bef4d410718a71c4b3
马小雅 <maxiaoya@tuhu.cn>
2025-07-16 20:27:02 +0800

    fix: typo

--- createCrx.js
+++ createCrx.js
@@ -55,7 +55,7 @@ async function buildCrx() {
 
         // 创建 CRX 实例
         const extension = new crx({
-            codebase: `auto-update-chrome-plugin.pages.dev/auto-update-server/${config.crxName}`, // 自动更新地址（可选）
+            codebase: `https://auto-update-chrome-plugin-1.pages.dev/auto-update-server/${config.crxName}`, // 自动更新地址（可选）
             privateKey
         });
 
